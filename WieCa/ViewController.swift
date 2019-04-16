da5f7c4b09d87aa7b6459d2a519762269516d65f
Indrat Saputra <indrat.saputra@icloud.com>
2019-04-16 13:34:58 +0700

    Initial Commit

---
+++ WieCa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WieCa
+//
+//  Created by IndratS on 16/04/19.
+//  Copyright © 2019 IndratS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
